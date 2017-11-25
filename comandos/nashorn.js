a2d1031254f02190ef901c5f38283ca1d81e20fb
BannerGames <davidandrade339@gmail.com>
2017-11-25 10:17:31 +0000

    Adição de uma explicação de cada coisa.
    Adição de uma explicação de cada coisa e fiz uma correção que não faz diferença: agrs passou a args.

--- comandos/nashorn.js
+++ comandos/nashorn.js
@@ -1,14 +1,14 @@
 var guild = getGuild();
 var role = guild.getRoleById("ID DA ROLE") //Id da role de staff
-    if (author().hasRole(role)) {
-         agrs = joinArguments()
-        var evalzado = eval(agrs); 
-        reply(evalzado) 
+    if (author().hasRole(role)) { // Se o usuário que executar tiver a role (cargo) executa o código abaixo.
+         args = joinArguments() // O que usuário escrever.
+        var evalzado = eval(args);  // Executa o código que o usuário escreveu.
+        reply(evalzado) // Envia o codigo executado.
     
 } else { 
-reply("**Sem permissão |** :no_good:")
+reply("**Sem permissão |** :no_good:") // Se o usuário não tiver permissão irá mandar a seguinte mensagem: Sem Permissão | 🙅.
     
 }
 //Lhermebr#0596
-//Creditos ao baleia_roxa
+//Créditos ao baleia_roxa
 //Descrição: Executa comandos de Nashorn diretamente do chat do Discord
